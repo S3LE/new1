4d61fe2bfd923e2dd05b9f024c7df01c29fecbbf
S3LE <127926191+S3LE@users.noreply.github.com>
2023-03-15 18:27:05 +0530

    commit
    Commit

---
+++ EntityContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Entities.Models
+{
+    public partial class EntityContext : DbContext
+    {
+        public EntityContext()
+        {
+        }
+
+        public EntityContext(DbContextOptions<EntityContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId)
+                    .HasName("PK__Product1__B40CC6CD73B27013");
+
+                entity.Property(e => e.Category)
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Color)
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+
+namespace Entities.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly EntityContext _context;
+
+        public ProductsController(EntityContext context)
+        {
+            _context = context;
+        }
+
+       
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            List<SelectListItem> Category = new List<SelectListItem>()
+            {
+                new SelectListItem { Text = "Electronics" },
+                new SelectListItem { Text = "Stationary" },
+                new SelectListItem {  Text = "Appearels" }
+            };
+            ViewBag.Category = Category;
+            return View();
+          
+        }
+
+   
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,Category,Color,UnitPrice,AvailableQuantity")] Products products)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(products);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+   
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products.FindAsync(id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,Category,Color,UnitPrice,AvailableQuantity")] Products products)
+        {
+            if (id != products.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(products);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductsExists(products.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(products);
+        }
+
+     
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return View(products);
+        }
+
+      
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var products = await _context.Products.FindAsync(id);
+            _context.Products.Remove(products);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductsExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
